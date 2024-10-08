0ef18d4a311e7e430cf77da4fe8d1184e33a2a7f
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2024-10-07 21:41:35 -0300

    feat: add validate check in distance in minutes

--- api-solid/src/services/validate-check-in.service.spec.ts
+++ api-solid/src/services/validate-check-in.service.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository';
-import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error';
 import { ResourceNotFoundError } from './errors/resource-not-found-error';
 import { ValidateCheckInService } from './validate-check-in.service';
 
@@ -11,11 +12,11 @@ describe('Validade Check In Service', () => {
     checkInsRepository = new InMemoryCheckInsRepository();
     sut = new ValidateCheckInService(checkInsRepository);
 
-    // vi.useFakeTimers();
+    vi.useFakeTimers();
   });
 
   afterEach(() => {
-    // vi.useRealTimers();
+    vi.useRealTimers();
   });
 
   it('should be able to validate the check-in', async () => {
@@ -36,5 +37,26 @@ describe('Validade Check In Service', () => {
     await expect(() =>
       sut.execute({ checkInId: 'inexistent-check-in-id' }),
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40));
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    });
+
+    const { checkIn } = await sut.execute({
+      checkInId: createdCheckIn.id,
+    });
+
+    const twentyOneMinutesInMs = 21 * 60 * 1000;
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs);
+
+    await expect(() =>
+      sut.execute({ checkInId: checkIn.id }),
+    ).rejects.toBeInstanceOf(LateCheckInValidationError);
   });
 });
