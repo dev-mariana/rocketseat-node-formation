546e0ef13a8e6ec0c25f0fe7ccb4661c55599f6e
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2024-10-17 17:59:10 -0300

    test: add gyms controller

---
+++ api-solid/src/http/controllers/gyms/nearby.controller.spec.ts
@@ -0,0 +1,57 @@
+import { app } from '@/app';
+import { createAndAuthenticateUser } from '@/utils/tests/create-and-authenticate-user';
+import request from 'supertest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+
+describe('Nearby Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able list nearby gyms', async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Some description.',
+        phone: '1199999999',
+        latitude: -22.9284506,
+        longitude: -43.1816704,
+      });
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'TypeScript Gym',
+        description: 'Some description.',
+        phone: '1199999999',
+        userLatitude: -23.0026692,
+        userLongitude: -43.3170944,
+      });
+
+    const response = await request(app.server)
+      .get('/gyms/nearby')
+      .query({
+        latitude: -22.9284506,
+        longitude: -43.1816704,
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send();
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.gyms).toHaveLength(1);
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'JavaScript Gym',
+      }),
+    ]);
+  });
+});
