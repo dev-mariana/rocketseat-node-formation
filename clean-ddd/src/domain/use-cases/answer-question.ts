eb35e32f4db2b24bdb8e2a883e66c62dd752b721
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2025-01-27 17:15:03 -0300

    feat: add entities creation abstracting

--- clean-ddd/src/domain/use-cases/answer-question.ts
+++ clean-ddd/src/domain/use-cases/answer-question.ts
@@ -1,17 +1,26 @@
+import { UniqueEntityId } from "../../core/entities/unique-entity-id";
 import { Answer } from "../entities/answer";
 import { AnswersRepository } from "../repositories/answers-repository";
 
-interface AnswerQuestionRequest {
+interface AnswerQuestionUseCaseRequest {
   instructorId: string;
   questionId: string;
   content: string;
 }
 
-export class AnswerQuestion {
+export class AnswerQuestionUseCase {
   constructor(private answersRepository: AnswersRepository) {}
 
-  async execute({ instructorId, questionId, content }: AnswerQuestionRequest) {
-    const answer = new Answer({ content, authorId: instructorId, questionId });
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest) {
+    const answer = Answer.create({
+      content,
+      authorId: new UniqueEntityId(instructorId),
+      questionId: new UniqueEntityId(questionId),
+    });
 
     await this.answersRepository.create(answer);
 
