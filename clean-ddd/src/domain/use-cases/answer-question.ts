3becb46333ea25bb54833bdcffe3316434c1afe3
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2025-01-27 17:32:20 -0300

    refactor: change name class to correct name

--- clean-ddd/src/domain/use-cases/answer-question.ts
+++ clean-ddd/src/domain/use-cases/answer-question.ts
@@ -2,20 +2,16 @@ import { UniqueEntityId } from "../../core/entities/unique-entity-id";
 import { Answer } from "../entities/answer";
 import { AnswersRepository } from "../repositories/answers-repository";
 
-interface AnswerQuestionUseCaseRequest {
+interface AnswerQuestionRequest {
   instructorId: string;
   questionId: string;
   content: string;
 }
 
-export class AnswerQuestionUseCase {
+export class AnswerQuestion {
   constructor(private answersRepository: AnswersRepository) {}
 
-  async execute({
-    instructorId,
-    questionId,
-    content,
-  }: AnswerQuestionUseCaseRequest) {
+  async execute({ instructorId, questionId, content }: AnswerQuestionRequest) {
     const answer = Answer.create({
       content,
       authorId: new UniqueEntityId(instructorId),
